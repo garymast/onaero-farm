0b19a8ed4f0c0527dd913e9dc678c8e9a14563bd
gary masterson <gary.masterson88@gmail.com>
2024-02-07 14:31:32 +0000

    Added bag comtext and delivery logic

--- onaero_farm/settings.py
+++ onaero_farm/settings.py
@@ -81,6 +81,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -191,3 +192,6 @@ if 'USE_AWS' in os.environ:
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
