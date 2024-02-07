280d43776ab09a3ab646207e334f04a7f468df04
gary masterson <gary.masterson88@gmail.com>
2024-02-07 17:34:18 +0000

    Add logic to change price by size

--- bag/contexts.py
+++ bag/contexts.py
@@ -23,6 +23,7 @@ def bag_contents(request):
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
+                # Add logic here to check for sizing prices
                 total += quantity * product.price
                 product_count += quantity
                 bag_items.append({
