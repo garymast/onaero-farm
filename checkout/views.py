d2e9bf8644f8eb3bc39e5ca24cef9a9a12234627
gary masterson <gary.masterson88@gmail.com>
2024-02-08 20:29:51 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OaxzbGM7C5XNjO8mEOP5GFFSwhI70Dt4yWzIMbsVB09iDqPnuGs1k9GXnxV0mCDVCsuh7am5RzAXWZub12JQ3pL00QNsMTTCy',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
