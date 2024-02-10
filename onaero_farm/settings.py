b772ebea86b52f617c72b7e99ff37a5fde41256f
gary masterson <gary.masterson88@gmail.com>
2024-02-10 11:50:05 +0000

    Completed email confirmations

--- onaero_farm/settings.py
+++ onaero_farm/settings.py
@@ -215,3 +215,5 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
+DEFUALT_FROM_EMAIL = 'onaerofarm@example.com'
+
