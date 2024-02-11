e001792613cd3e08c1e7325fa511aa370466c489
gary masterson <gary.masterson88@gmail.com>
2024-02-11 08:53:31 +0000

    Add hamburger menu for small screens

--- products/admin.py
+++ products/admin.py
@@ -3,24 +3,26 @@ from .models import Product, Category
 
 # Register your models here.
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
-        'sku',
-        'name',
-        'category',
-        'price',
-        'is_organic',
-        'origin',
-        'image',
+        "sku",
+        "name",
+        "category",
+        "price",
+        "is_organic",
+        "origin",
+        "image",
     )
 
-    ordering = ('name',)
+    ordering = ("name",)
+
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display=(
-        'friendly_name',
-        'name',
-        'cat_image',
+    list_display = (
+        "friendly_name",
+        "name",
+        "cat_image",
     )
 
 
